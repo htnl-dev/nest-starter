7913d4752ef4fe52d5e4bd7bf646f0eddce3afbf
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-12-11 00:51:50 +0100

    refactor(fsm): simplify InvalidTransitionException

--- src/crud/errors/fsm.errors.ts
+++ src/crud/errors/fsm.errors.ts
@@ -1,19 +1,7 @@
 import { ForbiddenException } from '@nestjs/common';
 
 export class InvalidTransitionException extends ForbiddenException {
-  constructor(currentStatus: string, nextStatus: string) {
-    super(
-      `Invalid transition`,
-      `Transition from ${currentStatus} to ${nextStatus} is not allowed`,
-    );
-  }
-}
-
-export class FinalStateTransitionException extends ForbiddenException {
-  constructor(currentStatus: string, nextStatus: string) {
-    super(
-      `Item is already in a final state: ${currentStatus}`,
-      `Transition from ${currentStatus} to ${nextStatus} is not allowed`,
-    );
+  constructor(from: string, to: string) {
+    super(`Transition from '${from}' to '${to}' is not allowed`);
   }
 }
