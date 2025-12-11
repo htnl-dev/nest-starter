21651ecc94c810fa2a48960982ee03ca82be3e49
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-12-11 17:28:39 +0100

    feat(crud): register new services in CrudModule

--- src/crud/crud.module.ts
+++ src/crud/crud.module.ts
@@ -1,8 +1,19 @@
 import { Module, Global } from '@nestjs/common';
+import { TransactionManager } from './services/transaction.manager';
+import { QueryBuilderService } from './services/query-builder.service';
+import { OptimisticLockingService } from './services/optimistic-locking.service';
+import { EntityEventEmitter } from './services/entity-event.emitter';
+
+const services = [
+  TransactionManager,
+  QueryBuilderService,
+  OptimisticLockingService,
+  EntityEventEmitter,
+];
 
 @Global()
 @Module({
-  providers: [],
-  exports: [],
+  providers: services,
+  exports: services,
 })
 export class CrudModule {}
