921c598c7a07adc40dc92910284f5740b6207574
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-12-11 17:28:56 +0100

    refactor(crud): update SlugCrudService constructor and remove console.log

--- src/crud/services/slug-crud.service.ts
+++ src/crud/services/slug-crud.service.ts
@@ -1,11 +1,14 @@
 import { AbstractCrudService } from './crud.service';
-import { ClientSession, Connection, isValidObjectId, Types } from 'mongoose';
-import { Model } from 'mongoose';
+import { ClientSession, isValidObjectId, Model, Types } from 'mongoose';
 import { UpdateCrudDto } from '../dto/update-crud.dto';
 import { slugify } from '../utils/slug.utils';
 import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { CreateSlugAwareCrudDto } from '../dto/create-slug-crud.dto';
 import { SlugCrudEntity } from '../entities/slug-crud.entity';
+import { TransactionManager } from './transaction.manager';
+import { QueryBuilderService } from './query-builder.service';
+import { OptimisticLockingService } from './optimistic-locking.service';
+import { EntityEventEmitter } from './entity-event.emitter';
 import type { CurrentUser } from '../types/current-user.type';
 
 export abstract class SlugAwareCrudService<
@@ -13,53 +16,68 @@ export abstract class SlugAwareCrudService<
   CreateDto extends { slug: string } = CreateSlugAwareCrudDto,
   UpdateDto extends object = UpdateCrudDto,
 > extends AbstractCrudService<Entity, CreateDto, UpdateDto> {
-  constructor(connection: Connection, model: Model<Entity>) {
-    super(connection, model);
+  constructor(
+    model: Model<Entity>,
+    transactionManager: TransactionManager,
+    queryBuilder: QueryBuilderService,
+    lockingService: OptimisticLockingService,
+    entityEvents: EntityEventEmitter,
+  ) {
+    super(
+      model,
+      transactionManager,
+      queryBuilder,
+      lockingService,
+      entityEvents,
+    );
   }
 
   slugSource: string = 'name';
 
-  protected getSlugFromSource(createCrudDto: CreateDto) {
-    const slugSource = (createCrudDto as Record<string, unknown>)[
+  protected getSlugFromSource(createDto: CreateDto): string {
+    const slugSource = (createDto as Record<string, unknown>)[
       this.slugSource
     ] as string | undefined;
+
     if (!slugSource) {
       throw new BadRequestException(`${this.slugSource} is not set`);
     }
+
     return slugify(slugSource);
   }
 
-  protected async generateUniqueSlug(createCrudDto: CreateDto) {
-    const slugSource = (createCrudDto as Record<string, unknown>)[
+  protected async generateUniqueSlug(createDto: CreateDto): Promise<string> {
+    const slugSource = (createDto as Record<string, unknown>)[
       this.slugSource
     ] as string | undefined;
+
     if (!slugSource) {
       throw new BadRequestException(`${this.slugSource} is not set`);
     }
 
-    let slug = this.getSlugFromSource(createCrudDto);
+    let slug = this.getSlugFromSource(createDto);
 
     while (await this.model.exists({ slug })) {
-      console.log('slug exists', slug);
       slug += '-' + Math.random().toString(36).substring(2, 15);
     }
+
     return slug;
   }
 
   async create(
-    createCrudDto: CreateDto,
+    createDto: CreateDto,
     user?: CurrentUser,
     session?: ClientSession,
   ) {
-    return this.withSession(session, async (session) => {
-      const slug = await this.generateUniqueSlug(createCrudDto);
-      createCrudDto.slug = slug;
-      return super.create(createCrudDto, user, session);
+    return this.transactionManager.withTransaction(session, async (session) => {
+      const slug = await this.generateUniqueSlug(createDto);
+      createDto.slug = slug;
+      return super.create(createDto, user, session);
     });
   }
 
   async findOne(idOrSlug: string | Types.ObjectId, session?: ClientSession) {
-    return this.withSession(session, async (session) => {
+    return this.transactionManager.withTransaction(session, async (session) => {
       if (isValidObjectId(idOrSlug)) {
         return super.findOne(idOrSlug, session);
       }
@@ -70,7 +88,7 @@ export abstract class SlugAwareCrudService<
         .session(session);
 
       if (!result) {
-        throw new NotFoundException('Not found');
+        throw new NotFoundException(`${this.modelName} not found`);
       }
 
       return result;
