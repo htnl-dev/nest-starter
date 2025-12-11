afb46e9176ad9dc7aec9cc42350f6798b7608b9e
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-12-11 17:49:11 +0100

    feat(app): register UserModule and LogtoModule with ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SeederModule } from './database/seeders/seeder.module';
 import { CrudModule } from './crud/crud.module';
+import { UserModule } from './user/user.module';
+import { LogtoModule } from './logto/logto.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGODB_URI!, {
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      family: 4,
-      maxPoolSize: 10,
-      minPoolSize: 5,
-      retryWrites: true,
-      retryReads: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+        family: 4,
+        maxPoolSize: 10,
+        minPoolSize: 5,
+        retryWrites: true,
+        retryReads: true,
+      }),
+      inject: [ConfigService],
     }),
     CrudModule,
+    LogtoModule,
+    UserModule,
     SeederModule,
   ],
   controllers: [AppController],
