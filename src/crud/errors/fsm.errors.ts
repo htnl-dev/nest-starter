1137f17b2da963569a811bfbaa4647132d3a9762
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-12-11 14:32:04 +0100

    refactor(crud): remove fsm errors

--- src/crud/errors/fsm.errors.ts
+++
@@ -1,7 +0,0 @@
-import { ForbiddenException } from '@nestjs/common';
-
-export class InvalidTransitionException extends ForbiddenException {
-  constructor(from: string, to: string) {
-    super(`Transition from '${from}' to '${to}' is not allowed`);
-  }
-}
