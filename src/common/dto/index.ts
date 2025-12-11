220ca8ac1e3b679ad499a81a676ea474a458f5c2
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-12-11 20:19:06 +0100

    feat(common): add barrel exports for cleaner imports

---
+++ src/common/dto/index.ts
@@ -0,0 +1,4 @@
+export * from './abstract-create.dto';
+export * from './abstract-update.dto';
+export * from './paginated-response.dto';
+export * from './query.dto';
---
+++ src/common/services/index.ts
@@ -0,0 +1,5 @@
+export * from './abstract.service';
+export * from './abstract.listener';
+export * from './abstract.processor';
+export * from './cache.service';
+export * from './transaction.manager';
